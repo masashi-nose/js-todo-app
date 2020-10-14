345f5f2931592338f3633d18ac11ca864f3b8e5d
nosemasashi <ma.nose9413@gmail.com>
2020-10-14 12:11:02 +0900

    build完了

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     // エントリーポイントの設定
-    entry: 'index.js',
+    entry: './index.js',
     // ビルド後、'./dist/my-bundle.js'というbundleファイルを生成する
     output: {
         path: path.resolve(__dirname, 'dist'),
